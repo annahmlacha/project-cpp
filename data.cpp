49be777c86b0f0462c43a37669da1f33493cf7fd
BONIGRAPHICS <Bonimniko2006@gmail.com>
2024-12-09 11:56:56 +0300

    sum operation aded

--- data.cpp
+++ data.cpp
@@ -5,6 +5,9 @@ int main() {
   cout << "Hello World!";
   int numberOne = 6;
   int numberTwo = 9;
+  int sum = numberOne + numberTwo;
   cout << "you have " << numberOne << "and" << numberTwo;
+  cout << "your sum is " << sum;
+
   return 0;
 }
